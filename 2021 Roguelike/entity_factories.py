19ebb4f8255f8db5e8ca1f5fdd279f8a69d89d5f
RivalRoman <86813819+RivalRoman@users.noreply.github.com>
2021-07-17 12:20:05 -0700

    Add files via upload

---
+++ 2021 Roguelike/entity_factories.py
@@ -0,0 +1,6 @@
+from entity import Entity
+
+player = Entity(char="@", color=(255, 255, 255), name= "Player", blocks_movement=True)
+
+punk = Entity(char="o", color=(63, 127, 63), name="Punk", blocks_movement=True)
+thug = Entity(char="T", color=(0, 127, 0), name="Thug", blocks_movement=True)
